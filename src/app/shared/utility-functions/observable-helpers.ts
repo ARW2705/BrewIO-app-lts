9b9cd1757258e3e9bdcf709b6cc7ab1f7d5f87da
Andrew Wanex <awanex2705@gmail.com>
2021-02-01 14:05:45 -0800

    feat: add array of subjects to array of values function

--- src/app/shared/utility-functions/observable-helpers.ts
+++ src/app/shared/utility-functions/observable-helpers.ts
@@ -17,6 +17,18 @@ export function getArrayFromObservables<T>(obsArr: Observable<T>[]): T[] {
 }
 
 /**
+ * Convert an array of behavior subjects to an array of the current value of
+ * those subjects
+ *
+ * @params: subArr - an array of behavior subjects of an object
+ *
+ * @return: new array of the current values of each behavior subject
+ */
+export function getArrayFromSubjects<T>(subArr: BehaviorSubject<T>[]): T[] {
+  return subArr.map((sub: BehaviorSubject<T>): T => sub.value);
+}
+
+/**
  * Convert an array into an array of behavior subjects
  *
  * @params: array - array to convert
