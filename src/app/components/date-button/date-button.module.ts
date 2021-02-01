98966a9a6a0e5f6201f1edbd0fa1ed4dda2895ef
Andrew Wanex <awanex2705@gmail.com>
2021-02-01 14:10:52 -0800

    doc: add structure comments

--- src/app/components/date-button/date-button.module.ts
+++ src/app/components/date-button/date-button.module.ts
@@ -1,10 +1,12 @@
+/* Module imports */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { PipesModule } from '../../pipes/pipes.module';
 
+/* Component imports */
 import { DateButtonComponent } from './date-button.component';
 
-import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   imports: [
