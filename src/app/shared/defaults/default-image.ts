c9fd212a368f75b5584ec5fb282ce33bcaa88272
Andrew Wanex <awanex2705@gmail.com>
2021-02-01 16:31:04 -0800

    refactor: use missing image url constant in default image

--- src/app/shared/defaults/default-image.ts
+++ src/app/shared/defaults/default-image.ts
@@ -1,10 +1,12 @@
+import { MISSING_IMAGE_URL } from '../constants/missing-image-url';
+
 import { Image } from '../interfaces/image';
 
 export const defaultImage: () => Image = () => {
   const def: Image = {
     cid: '0',
-    localURL: 'assets/imgs/no-label-image-240.png',
-    url: 'assets/imgs/no-label-image-240.png',
+    localURL: MISSING_IMAGE_URL,
+    url: MISSING_IMAGE_URL,
     serverFilename: 'missing'
   };
   return def;
