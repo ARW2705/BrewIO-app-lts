439d3c9c8493c114b65be3e5f31a41b1e9ac64da
Andrew Wanex <awanex2705@gmail.com>
2021-03-09 11:16:40 -0800

    chore: remove unused function

--- src/app/shared/utility-functions/observable-helpers.ts
+++ src/app/shared/utility-functions/observable-helpers.ts
@@ -1,20 +1,4 @@
-import { BehaviorSubject, Observable } from 'rxjs';
-
-/**
- * Convert an array of observables to an array of the current value of those
- * observables
- *
- * @params: obsArr - an array of observables of an object
- *
- * @return: new array of the current values of each observable
- */
-export function getArrayFromObservables<T>(obsArr: Observable<T>[]): T[] {
-  return obsArr.map((obs: Observable<T>) => {
-    let object: T = null;
-    obs.subscribe((data) => object = data);
-    return object;
-  });
-}
+import { BehaviorSubject } from 'rxjs';
 
 /**
  * Convert an array of behavior subjects to an array of the current value of
@@ -36,5 +20,5 @@ export function getArrayFromSubjects<T>(subArr: BehaviorSubject<T>[]): T[] {
  * @return: array of behavior subjects
  */
 export function toSubjectArray<T>(array: T[]): BehaviorSubject<T>[] {
-  return array.map((item) => new BehaviorSubject<T>(item));
+  return array.map((item: T): BehaviorSubject<T> => new BehaviorSubject<T>(item));
 }
