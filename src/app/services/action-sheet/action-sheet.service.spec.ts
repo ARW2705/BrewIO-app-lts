88d8103c467dbfd9d74fd45b63f2abfd7db00c2a
Andrew Wanex <awanex2705@gmail.com>
2021-03-30 20:43:51 -0700

    test: add action sheet service testing

---
+++ src/app/services/action-sheet/action-sheet.service.spec.ts
@@ -0,0 +1,90 @@
+/* Module imports */
+import { TestBed, getTestBed, async } from '@angular/core/testing';
+import { ActionSheetController } from '@ionic/angular';
+
+/* Test configuration imports */
+import { configureTestBed } from '../../../../test-config/configure-test-bed';
+
+/* Mock imports */
+import { mockActionSheetButtons } from '../../../../test-config/mock-models/mock-action-sheet-buttons';
+import { mockActionSheetElement } from '../../../../test-config/mock-models/mock-action-sheet-element';
+import { ActionSheetControllerMock, ActionSheetMock } from '../../../../test-config/mocks-ionic';
+
+/* Interface imports */
+import { ActionSheetButton } from '../../shared/interfaces/action-sheet-buttons';
+
+/* Service imports */
+import { ActionSheetService } from './action-sheet.service';
+
+
+describe('ActionSheetService', () => {
+
+  let injector: TestBed;
+  let actionSheetService: ActionSheetService;
+  let actionCtrl: ActionSheetController;
+  configureTestBed();
+
+  beforeAll(async(() => {
+    TestBed.configureTestingModule({
+      imports : [],
+      providers: [
+        ActionSheetService,
+        { provide: ActionSheetController, useClass: ActionSheetControllerMock }
+      ]
+    });
+
+    injector = getTestBed();
+    actionSheetService = injector.get(ActionSheetService);
+    actionCtrl = injector.get(ActionSheetController);
+  }));
+
+  test('should open action sheet with default options', (done: jest.DoneCallback) => {
+    const _mockActionSheetElement = mockActionSheetElement();
+
+    actionCtrl.create = jest
+      .fn()
+      .mockReturnValue(Promise.resolve(_mockActionSheetElement));
+
+    const createSpy: jest.SpyInstance = jest.spyOn(actionCtrl, 'create');
+    const presentSpy: jest.SpyInstance = jest.spyOn(_mockActionSheetElement, 'present');
+
+    actionSheetService.openActionSheet('test', []);
+
+    setTimeout(() => {
+      expect(presentSpy).toHaveBeenCalled();
+      const caughtCall: object = createSpy.mock.calls[0][0];
+      expect(caughtCall['header']).toMatch('test');
+      expect(caughtCall['buttons'].length).toEqual(1);
+      expect(caughtCall['buttons'][0]['text']).toMatch('Cancel');
+      expect(caughtCall['cssClass']).toMatch('action-sheet-main');
+      done();
+    }, 10);
+  });
+
+  test('should open action sheet with additional buttons', (done: jest.DoneCallback) => {
+    const _mockActionSheetElement = mockActionSheetElement();
+
+    actionCtrl.create = jest
+      .fn()
+      .mockReturnValue(Promise.resolve(_mockActionSheetElement));
+
+    const createSpy: jest.SpyInstance = jest.spyOn(actionCtrl, 'create');
+    const presentSpy: jest.SpyInstance = jest.spyOn(_mockActionSheetElement, 'present');
+
+    const _mockActionSheetButtons: ActionSheetButton[] = mockActionSheetButtons();
+
+    actionSheetService.openActionSheet('test', _mockActionSheetButtons, 'custom-class');
+
+    setTimeout(() => {
+      expect(presentSpy).toHaveBeenCalled();
+      const caughtCall: object = createSpy.mock.calls[0][0];
+      expect(caughtCall['header']).toMatch('test');
+      expect(caughtCall['buttons'].length).toEqual(5);
+      expect(caughtCall['buttons'][0]['text']).toMatch('Choice 1');
+      expect(caughtCall['buttons'][3]['role']).toMatch('role');
+      expect(caughtCall['buttons'][4]['text']).toMatch('Cancel');
+      done();
+    }, 10);
+  });
+
+});
