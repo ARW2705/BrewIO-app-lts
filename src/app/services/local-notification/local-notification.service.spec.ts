0a649a529aac3d63fcef6661a86341b2d81841e8
Andrew Wanex <awanex2705@gmail.com>
2021-04-13 16:28:37 -0700

    test: add local-notification service test suite

---
+++ src/app/services/local-notification/local-notification.service.spec.ts
@@ -0,0 +1,48 @@
+/* Module imports */
+import { TestBed, getTestBed, async } from '@angular/core/testing';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+
+/* TestBed configuration imports */
+import { configureTestBed } from '../../../../test-config/configure-test-bed';
+
+/* Mock imports */
+import { LocalNotificationsMock } from '../../../../test-config/mocks-ionic';
+
+/* Service imports */
+import { LocalNotificationService } from './local-notification.service';
+
+
+describe('LocalNotificationService', (): void => {
+  let injector: TestBed;
+  let localNotificationService: LocalNotificationService;
+  configureTestBed();
+
+  beforeAll(async((): void => {
+    TestBed.configureTestingModule({
+      providers: [
+        LocalNotificationService,
+        { provide: LocalNotifications, useClass: LocalNotificationsMock }
+      ]
+    });
+
+    injector = getTestBed();
+    localNotificationService = injector.get(LocalNotificationService);
+  }));
+
+  test('should create the service', (): void => {
+    expect(localNotificationService).toBeDefined();
+  });
+
+  test('should set a local notification', (): void => {
+    const localSpy: jest.SpyInstance = jest.spyOn(localNotificationService.localNotifications, 'schedule');
+
+    localNotificationService.setLocalNotification('test-title', 'optional-text');
+
+    expect(localSpy).toHaveBeenCalledWith({
+      title: 'test-title',
+      foreground: true,
+      color: '40e0cf',
+      text: 'optional-text'
+    });
+  });
+});
