66d88a88f17da455f8d1cd91ed6642b6361f4428
Andrew Wanex <awanex2705@gmail.com>
2021-04-26 13:21:22 -0700

    test: add ingredient list component test suite

---
+++ src/app/components/ingredient-list/ingredient-list.component.spec.ts
@@ -0,0 +1,86 @@
+/* Module imports */
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+/* Test configuration imports */
+import { configureTestBed } from '../../../../test-config/configure-test-bed';
+
+/* Mock imports */
+import { mockRecipeVariantComplete } from '../../../../test-config/mock-models/mock-recipe';
+import { UnitConversionPipeMock } from '../../../../test-config/mock-pipes/mock-unit-conversion-pipe';
+
+/* Interface imports */
+import { RecipeVariant } from '../../shared/interfaces/recipe-variant';
+
+/* Component imports */
+import { IngredientListComponent } from './ingredient-list.component';
+
+
+describe('IngredientListComponent', (): void => {
+  let fixture: ComponentFixture<IngredientListComponent>;
+  let ilCmp: IngredientListComponent;
+  configureTestBed();
+
+  beforeAll((done: any): Promise<void> => (async (): Promise<void> => {
+    TestBed.configureTestingModule({
+      declarations: [
+        IngredientListComponent,
+        UnitConversionPipeMock
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    });
+    await TestBed.compileComponents();
+  })()
+  .then(done)
+  .catch(done.fail));
+
+  beforeEach((): void => {
+    fixture = TestBed.createComponent(IngredientListComponent);
+    ilCmp = fixture.componentInstance;
+  });
+
+  test('should create the component', (): void => {
+    fixture.detectChanges();
+
+    expect(ilCmp).toBeDefined();
+  });
+
+  test('should display lists of ingredients of a recipe', (): void => {
+    const _mockRecipeVariant: RecipeVariant = mockRecipeVariantComplete();
+    const grainsCount: number = _mockRecipeVariant.grains.length;
+    const hopsCount: number = _mockRecipeVariant.hops.length;
+    const yeastCount: number = _mockRecipeVariant.yeast.length;
+    const otherCount: number = _mockRecipeVariant.otherIngredients.length;
+
+    ilCmp.recipeVariant = _mockRecipeVariant;
+
+    fixture.detectChanges();
+
+    const ingredientLists: NodeList = fixture.nativeElement.querySelectorAll('.ingredient-row');
+
+    // grain nodes
+    for (let i = 0; i < grainsCount; i++) {
+      const grainNodes: NodeList = ingredientLists.item(i).childNodes;
+      expect(grainNodes.item(0).textContent).toMatch(_mockRecipeVariant.grains[i].grainType.name);
+    }
+
+    // hops nodes
+    for (let i = 0; i < hopsCount; i++) {
+      const hopsNodes: NodeList = ingredientLists.item(grainsCount + i).childNodes;
+      expect(hopsNodes.item(0).textContent).toMatch(_mockRecipeVariant.hops[i].hopsType.name);
+    }
+
+    // yeast nodes
+    for (let i = 0; i < yeastCount; i++) {
+      const yeastNodes: NodeList = ingredientLists.item(grainsCount + hopsCount + i).childNodes;
+      expect(yeastNodes.item(0).textContent).toMatch(_mockRecipeVariant.yeast[i].yeastType.name);
+    }
+
+    // other ingredient nodes
+    for (let i = 0; i < yeastCount; i++) {
+      const otherNodes: NodeList = ingredientLists.item(grainsCount + hopsCount + yeastCount + i).childNodes;
+      expect(otherNodes.item(0).textContent).toMatch(_mockRecipeVariant.otherIngredients[i].name);
+    }
+  });
+
+});
