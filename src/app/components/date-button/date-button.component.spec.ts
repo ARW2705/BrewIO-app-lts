3cbe7e03a94c3953f0e3eb37c41021a085532e2b
Andrew Wanex <awanex2705@gmail.com>
2021-04-26 13:19:59 -0700

    test: add date button component test suite

---
+++ src/app/components/date-button/date-button.component.spec.ts
@@ -0,0 +1,77 @@
+/* Module imports */
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, SimpleChange, SimpleChanges } from '@angular/core';
+import * as moment from 'moment';
+
+/* Test configuration imports */
+import { configureTestBed } from '../../../../test-config/configure-test-bed';
+
+/* Mock imports */
+import { MomentPipeMock } from '../../../../test-config/mock-pipes/mock-moment-pipe';
+
+/* Component imports */
+import { DateButtonComponent } from './date-button.component';
+
+
+describe('DateButtonComponent', (): void => {
+  let fixture: ComponentFixture<DateButtonComponent>;
+  let dbCmp: DateButtonComponent;
+  configureTestBed();
+
+  beforeAll((done: any): Promise<void> => (async (): Promise<void> => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DateButtonComponent,
+        MomentPipeMock
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    });
+    await TestBed.compileComponents();
+  })()
+  .then(done)
+  .catch(done.fail));
+
+  beforeEach((): void => {
+    fixture = TestBed.createComponent(DateButtonComponent);
+    dbCmp = fixture.componentInstance;
+  });
+
+  test('should create the component', (): void => {
+    fixture.detectChanges();
+
+    expect(dbCmp).toBeDefined();
+  });
+
+  test('should handle input changes', (): void => {
+    const startChange: SimpleChanges = { isStart: new SimpleChange(null, true, true) };
+    const projectedChange: SimpleChanges = { isProjected: new SimpleChange(null, true, true) };
+    const monthChange: SimpleChanges = { isMonth: new SimpleChange(null, true, true) };
+
+    fixture.detectChanges();
+
+    expect(dbCmp.svgClass).toMatch('base');
+
+    dbCmp.ngOnChanges(startChange);
+
+    expect(dbCmp.svgClass).toMatch('base start');
+
+    dbCmp.ngOnChanges(projectedChange);
+
+    expect(dbCmp.svgClass).toMatch('base projected');
+
+    dbCmp.ngOnChanges(monthChange);
+
+    expect(dbCmp.svgClass).toMatch('base month');
+  });
+
+  test('should display the date', (): void => {
+    const testMoment: moment.Moment = moment('2020-01-01');
+    dbCmp.date = testMoment;
+
+    fixture.detectChanges();
+
+    const button: HTMLElement = fixture.nativeElement.querySelector('text');
+    expect(button.textContent).toMatch(testMoment.toISOString());
+  });
+
+});
