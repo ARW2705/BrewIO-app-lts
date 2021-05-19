f22532337524e98ba623099f3a0f02eb6772c1f8
Andrew Wanex <awanex2705@gmail.com>
2021-05-19 12:09:12 -0700

    chore: remove unused imports

--- src/app/pages/forms/image-form/image-form.module.ts
+++ src/app/pages/forms/image-form/image-form.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { ImageFormPage } from './image-form.page';
 
-import { FormErrorComponentModule } from '../../../components/form-error/form-error.module';
 import { HeaderComponentModule } from '../../../components/header/header.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
     IonicModule,
-    FormErrorComponentModule,
     HeaderComponentModule
   ],
   declarations: [ImageFormPage]
