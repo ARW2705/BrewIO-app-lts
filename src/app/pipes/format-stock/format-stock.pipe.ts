f820ce1f3cf58f2ef7e141d74c6e24503cf9fdff
Andrew Wanex <awanex2705@gmail.com>
2021-05-24 17:22:02 -0700

    chore: add documentation

--- src/app/pipes/format-stock/format-stock.pipe.ts
+++ src/app/pipes/format-stock/format-stock.pipe.ts
@@ -15,7 +15,13 @@ import { StockType } from '../../shared/interfaces/stocktype';
 export class FormatStockPipe implements PipeTransform {
 
   /**
+   * Format stock output based on type
    *
+   * @params: item - parent item
+   * @params: type - transform either the stock value or name
+   * @params: [refresh] - used as flag for pipe to recalculate only
+   *
+   * @return: formatted stock value or name
    */
   transform(item: InventoryItem, type: string, refresh?: boolean): string {
     switch (type) {
@@ -28,20 +34,34 @@ export class FormatStockPipe implements PipeTransform {
     }
   }
 
+  /**
+   * Format the item quantity message depending on whether the stock type uses
+   * discrete units or is capacity based
+   *
+   * @params: item - the parent item
+   *
+   * @return: formatted quantity message as current units if in discrete units
+   * or as a percentage remaining if capacity based
+   */
   formatItemQuantityText(item: InventoryItem): string {
     const stockType: StockType = STOCK_TYPES
-      .find((type: StockType) => type.name === item.stockType);
+      .find((type: StockType): boolean => type.name === item.stockType);
 
     if (stockType.isDiscreteUnit) {
       return item.currentQuantity.toString();
     } else {
-      const remaining: number = Math.floor(
-        item.currentQuantity / item.initialQuantity * 100
-      );
+      const remaining: number = Math.floor(item.currentQuantity / item.initialQuantity * 100);
       return`${remaining}%`;
     }
   }
 
+  /**
+   * Format the stock type name with prefix name removed (e.g. 'Standard Can' becomes 'Can')
+   *
+   * @params: item - the parent item
+   *
+   * @return: the formatted stock type name
+   */
   formatItemTypeText(item: InventoryItem): string {
     const stockType: StockType = STOCK_TYPES
       .find((type: StockType) => type.name === item.stockType);
