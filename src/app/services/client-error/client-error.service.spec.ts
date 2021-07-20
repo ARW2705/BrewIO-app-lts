8ad4932a8f769a96ddb01d761cb15fbfdc647bd1
Andrew Wanex <awanex2705@gmail.com>
2021-07-19 18:11:49 -0700

    feat: add ErrorHandler override service

---
+++ src/app/services/client-error/client-error.service.spec.ts
@@ -0,0 +1,150 @@
+/* Module imports */
+import { TestBed, async, getTestBed } from '@angular/core/testing';
+
+/* Test configuration imports */
+import { configureTestBed } from '../../../../test-config/configure-test-bed';
+
+/* Mock imports */
+import { ErrorReportingServiceStub } from '../../../../test-config/service-stubs';
+
+/* Type imports */
+import { CustomError } from '../../shared/types';
+
+/* Service imports */
+import { ClientErrorService } from './client-error.service';
+import { ErrorReportingService } from '../error-reporting/error-reporting.service';
+
+
+describe('ClientErrorService', () => {
+  let injector: TestBed;
+  let clientError: ClientErrorService;
+  let errorReporter: ErrorReportingService;
+  configureTestBed();
+
+  beforeAll(async((): void => {
+    TestBed.configureTestingModule({
+      providers: [
+        ClientErrorService,
+        { provide: ErrorReportingService, useClass: ErrorReportingServiceStub }
+      ]
+    });
+  }));
+
+  beforeEach((): void => {
+    injector = getTestBed();
+    clientError = injector.get(ClientErrorService);
+    errorReporter = injector.get(ErrorReportingService);
+    errorReporter.setErrorReport = jest.fn();
+    errorReporter.getTimestamp = jest
+      .fn()
+      .mockReturnValue('timestamp');
+    clientError.injector.get = jest
+      .fn()
+      .mockReturnValue(errorReporter);
+  });
+
+  test('should create the service', (): void => {
+    expect(clientError).toBeTruthy();
+  });
+
+  test('should handle an error', (): void => {
+    const _mockError: Error = new Error('test-error');
+
+    clientError.createJSErrorReport = jest.fn();
+    clientError.createUnknownErrorReport = jest.fn();
+
+    const setSpy: jest.SpyInstance = jest.spyOn(errorReporter, 'setErrorReport');
+    const createSpy: jest.SpyInstance = jest.spyOn(clientError, 'createJSErrorReport');
+
+    clientError.handleError(_mockError);
+
+    expect(setSpy).toHaveBeenCalled();
+    expect(createSpy).toHaveBeenCalledWith(_mockError, 'timestamp');
+  });
+
+  test('should handle an error of unknown type', (): void => {
+    const _mockUnknown: any = { test: true };
+
+    clientError.createJSErrorReport = jest.fn();
+    clientError.createUnknownErrorReport = jest.fn();
+
+    const setSpy: jest.SpyInstance = jest.spyOn(errorReporter, 'setErrorReport');
+    const createSpy: jest.SpyInstance = jest.spyOn(clientError, 'createUnknownErrorReport');
+
+    clientError.handleError(_mockUnknown);
+
+    expect(setSpy).toHaveBeenCalled();
+    expect(createSpy).toHaveBeenCalledWith(_mockUnknown, 'timestamp');
+  });
+
+  test('should create a JS Error report', (): void => {
+    const _mockError: Error = new Error();
+    _mockError.name = 'ErrorName';
+    _mockError.message = 'test-error';
+
+    const _mockCustomError: CustomError = new CustomError(
+      'CustomName',
+      'test-message',
+      3,
+      'test-user-message'
+    );
+
+    clientError.getUserMessage = jest
+      .fn()
+      .mockReturnValue('user-message');
+
+    const getSpy: jest.SpyInstance = jest.spyOn(clientError, 'getUserMessage');
+
+    expect(clientError.createJSErrorReport(_mockError, 'timestamp')).toStrictEqual({
+      message: 'test-error',
+      name: 'ErrorName',
+      severity: 2,
+      stackTrace: _mockError.stack,
+      timestamp: 'timestamp',
+      userMessage: 'user-message'
+    });
+    expect(getSpy).toHaveBeenNthCalledWith(1, _mockError);
+
+    expect(clientError.createJSErrorReport(_mockCustomError, 'timestamp')).toStrictEqual({
+      message: 'test-message',
+      name: 'CustomName',
+      severity: 3,
+      stackTrace: _mockCustomError.stack,
+      timestamp: 'timestamp',
+      userMessage: 'user-message'
+    });
+    expect(getSpy).toHaveBeenNthCalledWith(2, _mockCustomError);
+  });
+
+  test('should create an unknown error report', (): void => {
+    const _mockUnknown: any = { test: true };
+
+    clientError.getUserMessage = jest
+      .fn()
+      .mockReturnValue('user-message');
+
+    expect(clientError.createUnknownErrorReport(_mockUnknown, 'timestamp')).toStrictEqual({
+      name: 'UnknownError',
+      message: '{"test":true}',
+      severity: 2,
+      timestamp: 'timestamp',
+      userMessage: 'user-message'
+    });
+  });
+
+  test('should get a user message', (): void => {
+    const _mockError: Error = new Error();
+    _mockError.name = 'TypeError';
+
+    const _mockCustomError: CustomError = new CustomError('CustomError', 'message', 2, 'user-message');
+
+    const _mockUnknownButMessage: any = { message: 'test-message' };
+
+    expect(clientError.getUserMessage(_mockError)).toMatch('A system error has occurred');
+    expect(clientError.getUserMessage(_mockCustomError)).toMatch(_mockCustomError.userMessage);
+    expect(clientError.getUserMessage(_mockUnknownButMessage)).toMatch('test-message');
+    expect(typeof clientError.getUserMessage({})).toMatch('string');
+    expect(clientError.getUserMessage({}).length).toEqual(0);
+  });
+
+});
---
+++ src/app/services/client-error/client-error.service.ts
@@ -0,0 +1,93 @@
+/* Module imports */
+import { Injectable, Injector, ErrorHandler } from '@angular/core';
+
+/* Constant imports */
+import { BUILTIN_ERROR_TYPES } from '../../shared/constants';
+
+/* Interface imports */
+import { ErrorReport } from '../../shared/interfaces';
+
+/* Service imports */
+import { ErrorReportingService } from '../error-reporting/error-reporting.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientErrorService implements ErrorHandler {
+
+  constructor(public injector: Injector) {}
+
+  /**
+   * Handle a thrown error that has was not previously handled
+   *
+   * @param: error - the error to handle; expect type Error, but handle any
+   *
+   * @return: none
+   */
+  handleError(error: any): void {
+    const errorReporter: ErrorReportingService = this.injector.get(ErrorReportingService);
+    if (error instanceof Error) {
+      errorReporter.setErrorReport(this.createJSErrorReport(error, errorReporter.getTimestamp()));
+    } else {
+      errorReporter.setErrorReport(this.createUnknownErrorReport(error, errorReporter.getTimestamp()));
+    }
+  }
+
+  /**
+   * Create an error report from an Error
+   *
+   * @param: error - the caught error
+   * @param: timestamp - ISO string timestamp
+   *
+   * @return: a new error report
+   */
+  createJSErrorReport(error: Error, timestamp: string): ErrorReport {
+    return {
+      message: error.message,
+      name: error.name,
+      severity: error['severity'] || 2,
+      stackTrace: error.stack,
+      timestamp: timestamp,
+      userMessage: this.getUserMessage(error)
+    };
+  }
+
+  /**
+   * Create an error report from an unknown error type
+   *
+   * @param: error - the caught error
+   * @param: timestamp - ISO string timestamp
+   *
+   * @return: a new error report
+   */
+  createUnknownErrorReport(error: any, timestamp: string): ErrorReport {
+    return {
+      name: 'UnknownError',
+      message: JSON.stringify(error),
+      severity: 2,
+      timestamp: timestamp,
+      userMessage: this.getUserMessage(error)
+    };
+  }
+
+  /**
+   * Format an error message to present to the user
+   *
+   * @param: error - the caught error
+   *
+   * @return: an error message for the user; may be empty string
+   */
+  getUserMessage(error: any): string {
+    if (error && error['name'] && BUILTIN_ERROR_TYPES.includes(error.name)) {
+      return 'A system error has occurred';
+    } else if (error && error.hasOwnProperty('userMessage')) {
+      return error.userMessage;
+    } else if (error && error.message) {
+      return error.message;
+    } else {
+      return '';
+    }
+  }
+
+}
