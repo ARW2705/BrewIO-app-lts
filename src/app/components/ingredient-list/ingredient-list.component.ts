f4277514ab9f14387d6fc0d87468351eaba5a6c0
Andrew Wanex <awanex2705@gmail.com>
2021-07-22 15:44:17 -0700

    feat: add no-ingredients message to list if there are no ingredients of any kind

--- src/app/components/ingredient-list/ingredient-list.component.ts
+++ src/app/components/ingredient-list/ingredient-list.component.ts
@@ -1,5 +1,5 @@
 /* Module imports */
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 /* Interface imports */
 import { RecipeVariant } from '../../shared/interfaces';
@@ -10,10 +10,22 @@ import { RecipeVariant } from '../../shared/interfaces';
   templateUrl: './ingredient-list.component.html',
   styleUrls: ['./ingredient-list.component.scss'],
 })
-export class IngredientListComponent {
+export class IngredientListComponent implements OnChanges {
   @Input() recipeVariant: RecipeVariant = null;
   @Input() refreshPipes: boolean = false;
+  hasIngredients: boolean = false;
 
   constructor() { }
+
+  ngOnChanges(): void {
+    console.log('ingredient list changes');
+    this.hasIngredients = this.recipeVariant
+      && (
+        this.recipeVariant.grains.length > 0
+        || this.recipeVariant.hops.length > 0
+        || this.recipeVariant.yeast.length > 0
+        || this.recipeVariant.otherIngredients.length > 0
+      );
+  }
 
 }
