e79c0b6b9c3d44aad5fab8113ae6acdb19d763c3
Andrew Wanex <awanex2705@gmail.com>
2021-07-21 20:22:58 -0700

    test: add utility service stub

---
+++ test-config/service-stubs/utility-stub.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UtilityServiceStub {
+  public clone() {}
+  public compareWith() {}
+  public getArrayFromSubjects<T>() {}
+  public toSubjectArray<T>() {}
+  public roundToDecimalPlace() {}
+  public stripSharedProperties() {}
+  public toTitleCase() {}
+}
