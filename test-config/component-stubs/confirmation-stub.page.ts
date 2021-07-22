10c54b4ca6018b78a295c8e69b73097577c93f0b
Andrew Wanex <awanex2705@gmail.com>
2021-07-21 20:21:09 -0700

    refactor: rename confirmation stub from component to page to reflect changes to confirmation page

--- test-config/component-stubs/confirmation-stub.page.ts
+++ test-config/component-stubs/confirmation-stub.page.ts
@@ -2,16 +2,16 @@
 import { Component, Input } from '@angular/core';
 
 /* Component imports */
-import { ConfirmationComponent } from '../../src/app/components/confirmation/confirmation.component';
+import { ConfirmationPage } from '../../src/app/pages/confirmation/confirmation.page';
 
 @Component({
   selector: 'calendar',
   template: '',
   providers: [
-    { provide: ConfirmationComponent, useClass: ConfirmationComponentStub }
+    { provide: ConfirmationPage, useClass: ConfirmationPageStub }
   ]
 })
-export class ConfirmationComponentStub {
+export class ConfirmationPageStub {
   @Input() message: string = '';
   @Input() subMessage: string = null;
 }
