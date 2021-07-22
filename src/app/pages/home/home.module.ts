8e62232c3ad288b5cb597c2ff908a5cf99e6487c
Andrew Wanex <awanex2705@gmail.com>
2021-07-22 15:32:57 -0700

    bugfix: add error report page module to home page module imports to fix report page missing types

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -11,6 +11,7 @@ import { HeaderComponentModule } from '../../components/header/header.module';
 import { InventoryComponentModule } from '../../components/inventory/inventory.module';
 
 /* Page imports */
+import { ErrorReportPageModule } from '../error-report/error-report.module';
 import { HomePage } from './home.page';
 import { LoginPageModule } from '../forms/login/login.module';
 import { SignupPageModule } from '../forms/signup/signup.module';
@@ -19,6 +20,7 @@ import { SignupPageModule } from '../forms/signup/signup.module';
 @NgModule({
   imports: [
     CommonModule,
+    ErrorReportPageModule,
     LoginPageModule,
     SignupPageModule,
     IonicModule,
