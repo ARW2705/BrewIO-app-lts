8d60cfc4199a74bc57085a3b4a15048b7c24064a
Andrew Wanex <awanex2705@gmail.com>
2021-08-17 20:36:17 -0700

    chore: remove unused constructor

--- src/app/components/friends/friends.component.ts
+++ src/app/components/friends/friends.component.ts
@@ -1,12 +1,10 @@
+/* Module imports */
 import { Component } from '@angular/core';
 
+
 @Component({
-  selector: 'friends',
+  selector: 'app-friends',
   templateUrl: './friends.component.html',
   styleUrls: ['./friends.component.scss'],
 })
-export class FriendsComponent {
-
-  constructor() { }
-
-}
+export class FriendsComponent {}
