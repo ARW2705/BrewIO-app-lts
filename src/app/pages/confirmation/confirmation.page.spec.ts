11e3c3cb66dd3a9cb70cd0ead21c1389c0343060
Andrew Wanex <awanex2705@gmail.com>
2021-08-23 19:01:56 -0700

    chore: update formatting

--- src/app/pages/confirmation/confirmation.page.spec.ts
+++ src/app/pages/confirmation/confirmation.page.spec.ts
@@ -14,9 +14,9 @@ import { ConfirmationPage } from './confirmation.page';
 
 
 describe('ConfirmationPage', (): void => {
-  let fixture: ComponentFixture<ConfirmationPage>;
-  let confirmPage: ConfirmationPage;
   configureTestBed();
+  let fixture: ComponentFixture<ConfirmationPage>;
+  let page: ConfirmationPage;
 
   beforeAll((done: any): Promise<void> => (async (): Promise<void> => {
     TestBed.configureTestingModule({
@@ -33,47 +33,42 @@ describe('ConfirmationPage', (): void => {
 
   beforeEach((): void => {
     fixture = TestBed.createComponent(ConfirmationPage);
-    confirmPage = fixture.componentInstance;
+    page = fixture.componentInstance;
   });
 
   test('should create the component', (): void => {
     fixture.detectChanges();
 
-    expect(confirmPage).toBeDefined();
+    expect(page).toBeDefined();
   });
 
   test('should assign back click handler', (): void => {
     fixture.detectChanges();
 
-    expect(confirmPage.onBackClick).toBeDefined();
+    expect(page.onBackClick).toBeDefined();
   });
 
   test('should dismiss the modal with a result', (): void => {
-    confirmPage.modalCtrl.dismiss = jest
-      .fn();
-
-    const dismissSpy: jest.SpyInstance = jest.spyOn(confirmPage.modalCtrl, 'dismiss');
+    page.modalCtrl.dismiss = jest.fn();
+    const dismissSpy: jest.SpyInstance = jest.spyOn(page.modalCtrl, 'dismiss');
 
     fixture.detectChanges();
 
-    confirmPage.submit(true);
-
+    page.submit(true);
     expect(dismissSpy).toHaveBeenCalledWith(true);
   });
 
   test('should render the template', (): void => {
-    confirmPage.message = 'test message';
-    confirmPage.subMessage = 'test sub message';
+    page.message = 'test message';
+    page.subMessage = 'test sub message';
 
     fixture.detectChanges();
 
     const message: HTMLElement = fixture.nativeElement.querySelector('span');
     expect(message.textContent).toMatch('test message');
-
     const buttons: NodeList = fixture.nativeElement.querySelectorAll('ion-button');
     expect(buttons.item(0).textContent).toMatch('Cancel');
     expect(buttons.item(1).textContent).toMatch('Confirm');
-
     const subMessage: HTMLElement = fixture.nativeElement.querySelector('#sub-message');
     expect(subMessage.textContent).toMatch('test sub message');
   });
