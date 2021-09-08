ed47732e5413b4176bc2cfed78a011d96076ab79
Andrew Wanex <awanex2705@gmail.com>
2021-09-08 16:00:13 -0700

    feat: split slider content name into its own component

---
+++ src/app/components/recipe-slider-content-name/recipe-slider-content-name.component.spec.ts
@@ -0,0 +1,46 @@
+/* Module imports */
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+/* Test configuration imports */
+import { configureTestBed } from '../../../../test-config/configure-test-bed';
+
+/* Mock imports */
+import { mockRecipeMasterActive } from '../../../../test-config/mock-models';
+
+/* Interface imports */
+import { RecipeMaster } from '../../shared/interfaces';
+
+/* Component imports */
+import { RecipeSliderContentNameComponent } from './recipe-slider-content-name.component';
+
+
+describe('RecipeSliderContentNameComponent', (): void => {
+  let fixture: ComponentFixture<RecipeSliderContentNameComponent>;
+  let component: RecipeSliderContentNameComponent;
+  const _mockRecipeMasterActive: RecipeMaster = mockRecipeMasterActive();
+  configureTestBed();
+
+  beforeAll((done: any): Promise<void> => (async (): Promise<void> => {
+    TestBed.configureTestingModule({
+      declarations: [ RecipeSliderContentNameComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    });
+    await TestBed.compileComponents();
+  })()
+  .then(done)
+  .catch(done.fail));
+
+  beforeEach((): void => {
+    fixture = TestBed.createComponent(RecipeSliderContentNameComponent);
+    component = fixture.componentInstance;
+    component.recipe = _mockRecipeMasterActive;
+  });
+
+  test('should create the component', (): void => {
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+  });
+
+});
---
+++ src/app/components/recipe-slider-content-name/recipe-slider-content-name.component.ts
@@ -0,0 +1,16 @@
+/* Module imports */
+import { Component, Input } from '@angular/core';
+
+/* Interface imports */
+import { RecipeMaster } from '../../shared/interfaces';
+
+
+@Component({
+  selector: 'app-recipe-slider-content-name',
+  templateUrl: './recipe-slider-content-name.component.html',
+  styleUrls: ['./recipe-slider-content-name.component.scss'],
+})
+export class RecipeSliderContentNameComponent {
+  @Input() recipe: RecipeMaster;
+  @Input() refreshPipes: boolean;
+}
