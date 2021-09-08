1cc378384ada16bfdae3443d0f26d5b45221d0fb
Andrew Wanex <awanex2705@gmail.com>
2021-09-08 16:00:26 -0700

    feat: split slider content values into its own component

---
+++ src/app/components/recipe-slider-content-values/recipe-slider-content-values.component.spec.ts
@@ -0,0 +1,52 @@
+/* Module imports */
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+/* Test configuration imports */
+import { configureTestBed } from '../../../../test-config/configure-test-bed';
+
+/* Mock imports */
+import { mockRecipeVariantComplete } from '../../../../test-config/mock-models';
+import { RoundPipeStub, TruncatePipeStub, UnitConversionPipeStub } from '../../../../test-config/pipe-stubs';
+
+/* Interface imports */
+import { RecipeVariant } from '../../shared/interfaces';
+
+/* Component imports */
+import { RecipeSliderContentValuesComponent } from './recipe-slider-content-values.component';
+
+
+describe('RecipeSliderContentValuesComponent', (): void => {
+  let fixture: ComponentFixture<RecipeSliderContentValuesComponent>;
+  let component: RecipeSliderContentValuesComponent;
+  const _mockRecipeVariantComplete: RecipeVariant = mockRecipeVariantComplete();
+  configureTestBed();
+
+  beforeAll((done: any): Promise<void> => (async (): Promise<void> => {
+    TestBed.configureTestingModule({
+      declarations: [
+        RecipeSliderContentValuesComponent,
+        RoundPipeStub,
+        TruncatePipeStub,
+        UnitConversionPipeStub
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    });
+    await TestBed.compileComponents();
+  })()
+  .then(done)
+  .catch(done.fail));
+
+  beforeEach((): void => {
+    fixture = TestBed.createComponent(RecipeSliderContentValuesComponent);
+    component = fixture.componentInstance;
+    component.variant = _mockRecipeVariantComplete;
+  });
+
+  test('should create the component', (): void => {
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+  });
+
+});
---
+++ src/app/components/recipe-slider-content-values/recipe-slider-content-values.component.ts
@@ -0,0 +1,16 @@
+/* Module imports */
+import { Component, Input } from '@angular/core';
+
+/* Interface imports */
+import { RecipeVariant } from '../../shared/interfaces';
+
+
+@Component({
+  selector: 'app-recipe-slider-content-values',
+  templateUrl: './recipe-slider-content-values.component.html',
+  styleUrls: ['./recipe-slider-content-values.component.scss'],
+})
+export class RecipeSliderContentValuesComponent {
+  @Input() variant: RecipeVariant;
+  @Input() refreshPipes: boolean = false;
+}
