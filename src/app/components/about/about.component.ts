dc3520522ad31b077ed5866214786610a3f644e0
Andrew Wanex <awanex2705@gmail.com>
2021-09-08 16:48:02 -0700

    chore: set component properties as readonly

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -11,11 +11,8 @@ import { API_VERSION, APP_VERSION } from '../../shared/constants';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-  apiVersion: string = API_VERSION.split('v')[1];
-  appDescription: string = 'BrewIO is a multi-purpose tool to design homebrews, organize production, and track inventory.';
-  appVersion: string = APP_VERSION;
-  githubURL: string = 'https://github.com/ARW2705/BrewIO-App';
-
-  constructor() { }
-
+  readonly apiVersion: string = API_VERSION.split('v')[1];
+  readonly appDescription: string = 'BrewIO is a multi-purpose tool to design homebrews, organize production, and track inventory.';
+  readonly appVersion: string = APP_VERSION;
+  readonly githubURL: string = 'https://github.com/ARW2705/BrewIO-App';
 }
