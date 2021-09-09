269acdda557084a7f05a5adce5763fb72f184d68
Andrew Wanex <awanex2705@gmail.com>
2021-09-08 17:59:29 -0700

    chore: update imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 /* Module imports */
-import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ErrorHandler } from '@angular/core';
 import { RouteReuseStrategy } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { IonicStorageModule } from '@ionic/storage';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { BackgroundMode } from '@ionic-native/background-mode/ngx';
 import { Camera } from '@ionic-native/camera/ngx';
+import { Crop } from '@ionic-native/crop/ngx';
 import { Device } from '@ionic-native/device/ngx';
-import { File } from '@ionic-native/file/ngx';
 import { FilePath } from '@ionic-native/file-path/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { ImageResizer } from '@ionic-native/image-resizer/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 import { Network } from '@ionic-native/network/ngx';
-import { Crop } from '@ionic-native/crop/ngx';
-import { ImageResizer } from '@ionic-native/image-resizer/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage';
 
 /* App Modules */
 import { AppRoutingModule } from './app-routing.module';
@@ -31,6 +31,7 @@ import {
   AuthorizeInterceptor,
   BackgroundModeService,
   CalculationsService,
+  CalendarService,
   ClientErrorService,
   ConnectionService,
   DeviceService,
@@ -38,6 +39,7 @@ import {
   ErrorReportingService,
   EventService,
   FileService,
+  FormAttributeService,
   FormValidationService,
   HttpErrorService,
   IdService,
@@ -46,6 +48,7 @@ import {
   LibraryService,
   LocalNotificationService,
   LoggingService,
+  ModalService,
   PreferencesService,
   ProcessService,
   RecipeService,
@@ -64,57 +67,19 @@ import {
   entryComponents: [],
   imports: [
     AppRoutingModule,
-    BrowserModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
     HttpClientModule,
     IonicModule.forRoot({
       scrollAssist: false,
       scrollPadding: false
     }),
-    IonicStorageModule.forRoot()
+    IonicStorageModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [
-    Camera,
-    Crop,
-    Device,
-    File,
-    FilePath,
-    ImageResizer,
-    ImageService,
-    LocalNotifications,
-    StatusBar,
-    SplashScreen,
-    BackgroundMode,
-    Network,
-    WebView,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    ActionSheetService,
-    AnimationsService,
-    BackgroundModeService,
-    CalculationsService,
-    IdService,
-    ConnectionService,
-    DeviceService,
     { provide: ErrorHandler, useClass: ClientErrorService },
-    ErrorReportingService,
-    EventService,
-    FileService,
-    FormValidationService,
-    InventoryService,
-    LibraryService,
-    LocalNotificationService,
-    LoggingService,
-    PreferencesService,
-    HttpErrorService,
-    ProcessService,
-    RecipeService,
-    StorageService,
-    SyncService,
-    TimerService,
-    ToastService,
-    TypeGuardService,
-    UserService,
-    UtilityService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthorizeInterceptor,
@@ -124,7 +89,50 @@ import {
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorInterceptor,
       multi: true
-    }
+    },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ActionSheetService,
+    AnimationsService,
+    BackgroundMode,
+    BackgroundModeService,
+    CalculationsService,
+    CalendarService,
+    Camera,
+    ConnectionService,
+    Crop,
+    Device,
+    DeviceService,
+    ErrorReportingService,
+    EventService,
+    File,
+    FilePath,
+    FileService,
+    FormAttributeService,
+    FormValidationService,
+    HttpErrorService,
+    IdService,
+    ImageResizer,
+    ImageService,
+    InventoryService,
+    LibraryService,
+    LocalNotifications,
+    LocalNotificationService,
+    LoggingService,
+    ModalService,
+    PreferencesService,
+    ProcessService,
+    RecipeService,
+    StatusBar,
+    SplashScreen,
+    StorageService,
+    SyncService,
+    TimerService,
+    ToastService,
+    TypeGuardService,
+    UserService,
+    UtilityService,
+    Network,
+    WebView,
   ],
   bootstrap: [ AppComponent ]
 })
