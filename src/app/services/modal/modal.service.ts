1c49ef45d82775a97361de1d28f0a952228ce602
Andrew Wanex <awanex2705@gmail.com>
2021-09-08 17:35:05 -0700

    feat: create generic modal handling service

---
+++ src/app/services/modal/modal.service.ts
@@ -0,0 +1,74 @@
+/* Module imports */
+import { Injectable } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core';
+import { from, Observable, of } from 'rxjs';
+import { catchError, mergeMap } from 'rxjs/operators';
+
+/* Service imports */
+import { ErrorReportingService } from '../error-reporting/error-reporting.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+
+  constructor(
+    public errorReporter: ErrorReportingService,
+    public modalCtrl: ModalController
+  ) { }
+
+  /**
+   * Default modal onDidDismiss success handler
+   *
+   * @param: none
+   *
+   * @return: function that handles an overlay event
+   */
+  defaultSuccessHandler<T>(): (event: OverlayEventDetail<T>) => Observable<T> {
+    return (event: OverlayEventDetail<T>): Observable<T> => {
+      console.log('handling event', event);
+      return of(event.data);
+    };
+  }
+
+  /**
+   * Open an ionic modal
+   *
+   * @param: component - the host component for the modal
+   * @param: componentProps - contains key:value pairs to send to modal
+   * @param: [successHandler] - optional on modal dismiss success function;
+   *   defaults to function that returns result returned from modal as observable
+   * @param: [errorHandler] - optional on modal dismiss error function;
+   *   defaults to error reporting service generic error handler
+   *
+   * @return: observable of modal return data
+   */
+  openModal<M, T = M>(
+    component: any,
+    componentProps: { [key: string]: any },
+    successHandler: (event: OverlayEventDetail<M>) => Observable<M | T> = this.defaultSuccessHandler(),
+    errorHandler: (error: Error) => Observable<never> = this.errorReporter.handleGenericCatchError()
+  ): Observable<M | T> {
+    return from(this.openModalHelper<M>(component, componentProps))
+      .pipe(
+        mergeMap(successHandler),
+        catchError(errorHandler)
+      );
+  }
+
+  /**
+   * Ion modal helper function; set up modal and return its promise
+   *
+   * @param: component - the host component for the modal
+   * @param: componentProps - object containing key:value pairs to send to modal
+   *
+   * @return: modal onDidDismiss promise
+   */
+  async openModalHelper<M>(component: any, componentProps: object): Promise<OverlayEventDetail<M>> {
+    const modal: HTMLIonModalElement = await this.modalCtrl.create({ component, componentProps });
+    modal.present();
+    return modal.onDidDismiss<M>();
+  }
+}
