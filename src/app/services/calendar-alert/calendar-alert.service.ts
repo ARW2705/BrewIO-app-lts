d9dac5e3ce39bc6742571fbdf484c679dc80aea0
Andrew Wanex <awanex2705@gmail.com>
2021-09-08 17:34:12 -0700

    feat: extract calendar alert methods into its own service

---
+++ src/app/services/calendar-alert/calendar-alert.service.ts
@@ -0,0 +1,62 @@
+/* Module imports */
+import { Injectable } from '@angular/core';
+
+/* Interface imports */
+import { Alert, BatchProcess, CalendarMetadata, CalendarProcess } from '../../shared/interfaces';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalendarAlertService {
+
+  constructor() { }
+
+  /**
+   * Remove alerts for the current step
+   *
+   * @params: none
+   * @return: none
+   */
+  clearAlertsForCurrentStep(batchProcess: BatchProcess): void {
+    batchProcess.alerts = batchProcess.alerts.filter((alert: Alert): boolean => {
+      return alert.title !== batchProcess.schedule[batchProcess.currentStep].name;
+    });
+  }
+
+  /**
+   * Get alerts associated with the current step
+   *
+   * @params: none
+   *
+   * @return: Array of alerts
+   */
+  getAlerts(batchProcess: BatchProcess): Alert[] {
+    return batchProcess.alerts.filter((alert: Alert): boolean => {
+      return alert.title === batchProcess.schedule[batchProcess.currentStep].name;
+    });
+  }
+
+  /**
+   * Get alert for a particular step that is closest to the present datetime
+   *
+   * @params: none
+   *
+   * @return: alert that is closest to the current datetime
+   */
+  getClosestAlertByGroup(alerts: Alert[]): Alert {
+    if (alerts.length) {
+      const now: number = new Date().getTime();
+      return alerts.reduce(
+        (acc: Alert, curr: Alert): Alert => {
+          const accDiff: number = new Date(acc.datetime).getTime() - now;
+          const currDiff: number = new Date(curr.datetime).getTime() - now;
+          const isCurrCloser: boolean = Math.abs(currDiff) < Math.abs(accDiff) && currDiff > 0;
+          return isCurrCloser ? curr : acc;
+        }
+      );
+    }
+
+    return null;
+  }
+}
