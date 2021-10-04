7c1f69d3610f04d5c01ab2966a91049d0a9abe24
Andrew Wanex <awanex2705@gmail.com>
2021-10-04 15:30:35 -0700

    chore: update component imports

--- src/app/pages/extras/extras.module.ts
+++ src/app/pages/extras/extras.module.ts
@@ -1,22 +1,19 @@
 /* Module imports */
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
 /* Page imports */
-import { ExtrasPage } from './extras.page';
 import { LoginPageModule } from '../forms/login/login.module';
 import { SignupPageModule } from '../forms/signup/signup.module';
+import { ExtrasPage } from './extras.page';
 
 /* Component imports */
-import { AboutComponentModule } from '../../components/about/about.module';
-import { ActiveBatchListComponentModule } from '../../components/active-batch-list/active-batch-list.module';
-import { HeaderComponentModule } from '../../components/header/header.module';
-import { InventoryComponentModule } from '../../components/inventory/inventory.module';
-import { PreferencesComponentModule } from '../../components/preferences/preferences.module';
-import { UserComponentModule } from '../../components/user/user.module';
-
+import { ActiveBatchListComponentModule } from '../../components/batch/public';
+import { InventoryComponentModule } from '../../components/inventory/public';
+import { AboutComponentModule, HeaderComponentModule, PreferencesComponentModule } from '../../components/shared/public';
+import { UserComponentModule } from '../../components/user/public';
 
 @NgModule({
   imports: [
