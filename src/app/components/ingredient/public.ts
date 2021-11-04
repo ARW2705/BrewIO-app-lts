d7d2211c4d2efbacf2e7fa788b703b741094bd40
Andrew Wanex <awanex2705@gmail.com>
2021-11-04 16:57:47 -0700

    refactor: increase font size in ingredient item components

--- src/app/components/ingredient/public.ts
+++ src/app/components/ingredient/public.ts
@@ -1,15 +1,4 @@
-export { GrainBillComponentModule } from './public/grain-bill/grain-bill.module';
-export { GrainFormComponentModule } from './public/grain-form/grain-form.module';
-export { HopsFormComponentModule } from './public/hops-form/hops-form.module';
-export { HopsScheduleComponentModule } from './public/hops-schedule/hops-schedule.module';
+export { IngredientsContainerComponentModule } from './public/ingredients-container/ingredients-container.module';
 export { IngredientListComponentModule } from './public/ingredient-list/ingredient-list.module';
-export { OtherIngredientsComponentModule } from './public/other-ingredients/other-ingredients.module';
-export { OtherIngredientsFormComponentModule } from './public/other-ingredients-form/other-ingredients-form.module';
-export { YeastBatchComponentModule } from './public/yeast-batch/yeast-batch.module';
-export { YeastFormComponentModule } from './public/yeast-form/yeast-form.module';
 
-export { GrainFormComponent } from './public/grain-form/grain-form.component';
-export { HopsFormComponent } from './public/hops-form/hops-form.component';
 export { IngredientListComponent } from './public/ingredient-list/ingredient-list.component';
-export { OtherIngredientsFormComponent } from './public/other-ingredients-form/other-ingredients-form.component';
-export { YeastFormComponent } from './public/yeast-form/yeast-form.component';
