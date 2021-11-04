45070ff0471fa0d3f80c03f3cab5c50c776bcb0b
Andrew Wanex <awanex2705@gmail.com>
2021-11-04 16:58:31 -0700

    chore: update barrel export files

--- src/app/components/process/public.ts
+++ src/app/components/process/public.ts
@@ -1,17 +1,16 @@
 export { ProcessCalendarComponentModule } from './public/process-calendar/process-calendar.module';
-export { ProcessCalendarFormComponentModule } from './public/process-calendar-form/process-calendar-form.module';
+// export { ProcessCalendarFormComponentModule } from './public/process-calendar-form/process-calendar-form.module';
+export { ProcessContainerComponentModule } from './public/process-container/process-container.module';
 export { ProcessControlsComponentModule } from './public/process-controls/process-controls.module';
-export { ProcessListComponentModule } from './public/process-list/process-list.module';
 export { ProcessManualComponentModule } from './public/process-manual/process-manual.module';
-export { ProcessManualFormComponentModule } from './public/process-manual-form/process-manual-form.module';
+// export { ProcessManualFormComponentModule } from './public/process-manual-form/process-manual-form.module';
 export { ProcessTimerComponentModule } from './public/process-timer/process-timer.module';
-export { ProcessTimerFormComponentModule } from './public/process-timer-form/process-timer-form.module';
+// export { ProcessTimerFormComponentModule } from './public/process-timer-form/process-timer-form.module';
 
 export { ProcessCalendarComponent } from './public/process-calendar/process-calendar.component';
-export { ProcessCalendarFormComponent } from './public/process-calendar-form/process-calendar-form.component';
+// export { ProcessCalendarFormComponent } from './public/process-calendar-form/process-calendar-form.component';
 export { ProcessControlsComponent } from './public/process-controls/process-controls.component';
-export { ProcessListComponent } from './public/process-list/process-list.component';
 export { ProcessManualComponent } from './public/process-manual/process-manual.component';
-export { ProcessManualFormComponent } from './public/process-manual-form/process-manual-form.component';
+// export { ProcessManualFormComponent } from './public/process-manual-form/process-manual-form.component';
 export { ProcessTimerComponent } from './public/process-timer/process-timer.component';
-export { ProcessTimerFormComponent } from './public/process-timer-form/process-timer-form.component';
+// export { ProcessTimerFormComponent } from './public/process-timer-form/process-timer-form.component';
