aabf227fdb71cc5a7185d1d332df35233d81a4d0
Andrew Wanex <awanex2705@gmail.com>
2021-12-22 13:57:58 -0800

    chore: update process barrel file exports

--- src/app/components/process/public.ts
+++ src/app/components/process/public.ts
@@ -1,16 +1,12 @@
 export { ProcessCalendarComponentModule } from './public/process-calendar/process-calendar.module';
-// export { ProcessCalendarFormComponentModule } from './public/process-calendar-form/process-calendar-form.module';
 export { ProcessContainerComponentModule } from './public/process-container/process-container.module';
 export { ProcessControlsComponentModule } from './public/process-controls/process-controls.module';
 export { ProcessManualComponentModule } from './public/process-manual/process-manual.module';
-// export { ProcessManualFormComponentModule } from './public/process-manual-form/process-manual-form.module';
+export { ProcessMeasurementsFormComponentModule } from './public/process-measurements-form/process-measurements-form.module';
 export { ProcessTimerComponentModule } from './public/process-timer/process-timer.module';
-// export { ProcessTimerFormComponentModule } from './public/process-timer-form/process-timer-form.module';
 
 export { ProcessCalendarComponent } from './public/process-calendar/process-calendar.component';
-// export { ProcessCalendarFormComponent } from './public/process-calendar-form/process-calendar-form.component';
 export { ProcessControlsComponent } from './public/process-controls/process-controls.component';
 export { ProcessManualComponent } from './public/process-manual/process-manual.component';
-// export { ProcessManualFormComponent } from './public/process-manual-form/process-manual-form.component';
+export { ProcessMeasurementsFormComponent } from './public/process-measurements-form/process-measurements-form.component';
 export { ProcessTimerComponent } from './public/process-timer/process-timer.component';
-// export { ProcessTimerFormComponent } from './public/process-timer-form/process-timer-form.component';
