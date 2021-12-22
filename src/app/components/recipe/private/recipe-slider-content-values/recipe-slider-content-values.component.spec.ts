50451d675ad5ac1f3162f1496cc7f819a957019a
Andrew Wanex <awanex2705@gmail.com>
2021-12-22 13:49:54 -0800

    test: add template render test

--- src/app/components/recipe/private/recipe-slider-content-values/recipe-slider-content-values.component.spec.ts
+++ src/app/components/recipe/private/recipe-slider-content-values/recipe-slider-content-values.component.spec.ts
@@ -41,12 +41,28 @@ describe('RecipeSliderContentValuesComponent', (): void => {
     fixture = TestBed.createComponent(RecipeSliderContentValuesComponent);
     component = fixture.componentInstance;
     component.variant = _mockRecipeVariantComplete;
+    RoundPipeStub._returnValue = (value: number): number => value;
+    TruncatePipeStub._returnValue = (value: number): string => value.toString();
+    UnitConversionPipeStub._returnValue = (value: number, ...args: any[]): string => value.toString();
   });
 
   test('should create the component', (): void => {
     fixture.detectChanges();
-
     expect(component).toBeTruthy();
+  });
+
+  test('should render the template', (): void => {
+    fixture.detectChanges();
+
+    const spans: NodeList = fixture.nativeElement.querySelectorAll('span');
+    const volumeElem: Element = <Element>spans.item(1);
+    expect(volumeElem.textContent).toMatch(_mockRecipeVariantComplete.batchVolume.toString());
+    const abvElem: Element = <Element>spans.item(3);
+    expect(abvElem.textContent).toMatch(`${_mockRecipeVariantComplete.ABV}% ABV`);
+    const ibuElem: Element = <Element>spans.item(5);
+    expect(ibuElem.textContent).toMatch(`${_mockRecipeVariantComplete.IBU} IBU`);
+    const srmElem: Element = <Element>spans.item(7);
+    expect(srmElem.textContent).toMatch(`${_mockRecipeVariantComplete.SRM} SRM`);
   });
 
 });
