051189e817ab69db7cbc577fe161375b3ae6defa
Andrew Wanex <awanex2705@gmail.com>
2021-12-22 14:18:42 -0800

    chore: update formatting

--- src/app/components/note/public/note-form/note-form.component.spec.ts
+++ src/app/components/note/public/note-form/note-form.component.spec.ts
@@ -22,7 +22,7 @@ import { NoteFormComponent } from './note-form.component';
 describe('NoteFormComponent', (): void => {
   configureTestBed();
   let fixture: ComponentFixture<NoteFormComponent>;
-  let page: NoteFormComponent;
+  let component: NoteFormComponent;
   let originalOnInit: any;
 
   beforeAll((done: any): Promise<void> => (async (): Promise<void> => {
@@ -49,92 +49,89 @@ describe('NoteFormComponent', (): void => {
 
   beforeEach((): void => {
     fixture = TestBed.createComponent(NoteFormComponent);
-    page = fixture.componentInstance;
-    originalOnInit = page.ngOnInit;
-    page.ngOnInit = jest.fn();
-    page.modalCtrl.dismiss = jest.fn();
+    component = fixture.componentInstance;
+    originalOnInit = component.ngOnInit;
+    component.ngOnInit = jest.fn();
+    component.modalCtrl.dismiss = jest.fn();
   });
 
   test('should create the component', (): void => {
     fixture.detectChanges();
-
-    expect(page).toBeDefined();
+    expect(component).toBeTruthy();
   });
 
-  // test('should init the component', (): void => {
-  //   page.ngOnInit = originalOnInit;
-  //   page.noteType = 'master';
-  //   page.toUpdate = 'test note';
-  //   page.utilService.toTitleCase = jest.fn()
-  //     .mockReturnValue('Master Note');
-  //
-  //   fixture.detectChanges();
-  //
-  //   expect(page.title).toMatch('Master Note');
-  //   expect(page.note.value).toMatch('test note');
-  // });
-  //
-  // test('should dismiss modal with no data', (): void => {
-  //   page.modalCtrl.dismiss = jest.fn();
-  //   const dismissSpy: jest.SpyInstance = jest.spyOn(page.modalCtrl, 'dismiss');
-  //
-  //   fixture.detectChanges();
-  //
-  //   page.dismiss();
-  //   expect(dismissSpy).toHaveBeenCalled();
-  // });
-  //
-  // test('should handle note deletion modal dismiss', (): void => {
-  //   page.modalCtrl.dismiss = jest.fn();
-  //   const dismissSpy: jest.SpyInstance = jest.spyOn(page.modalCtrl, 'dismiss');
-  //
-  //   fixture.detectChanges();
-  //
-  //   page.onDelete();
-  //   expect(dismissSpy).toHaveBeenCalledWith({ method: 'delete' });
-  // });
-  //
-  // test('should handle note submission', (): void => {
-  //   page.note = new FormControl('test note');
-  //   page.formMethod = 'create';
-  //   page.modalCtrl.dismiss = jest.fn();
-  //   const dismissSpy: jest.SpyInstance = jest.spyOn(page.modalCtrl, 'dismiss');
-  //
-  //   fixture.detectChanges();
-  //
-  //   page.onSubmit();
-  //   expect(dismissSpy).toHaveBeenCalledWith({
-  //     method: 'create',
-  //     note: 'test note'
-  //   });
-  // });
-  //
-  // test('should render template with note form', (): void => {
-  //   page.note = new FormControl('test note');
-  //   page.formMethod = 'update';
-  //
-  //   fixture.detectChanges();
-  //
-  //   const textarea: HTMLElement = fixture.nativeElement.querySelector('app-form-text-area');
-  //   expect(textarea.getAttribute('controlName')).toMatch('note');
-  //   const formButtons: HTMLElement = fixture.nativeElement.querySelector('app-form-buttons');
-  //   expect(formButtons).toBeTruthy();
-  //   const deleteButton: HTMLElement = fixture.nativeElement.querySelector('app-delete-button');
-  //   expect(deleteButton).toBeTruthy();
-  // });
-  //
-  // test('should render template with delete button removed', (): void => {
-  //   page.note = new FormControl('');
-  //   page.formMethod = 'create';
-  //
-  //   fixture.detectChanges();
-  //
-  //   const textarea: HTMLElement = fixture.nativeElement.querySelector('app-form-text-area');
-  //   expect(textarea.getAttribute('controlName')).toMatch('note');
-  //   const formButtons: HTMLElement = fixture.nativeElement.querySelector('app-form-buttons');
-  //   expect(formButtons).toBeTruthy();
-  //   const deleteButton: HTMLElement = fixture.nativeElement.querySelector('app-delete-button');
-  //   expect(deleteButton).toBeNull();
-  // });
+  test('should init the component', (): void => {
+    component.ngOnInit = originalOnInit;
+    component.noteType = 'master';
+    component.toUpdate = 'test note';
+    component.utilService.toTitleCase = jest.fn()
+      .mockReturnValue('Master Note');
+
+    fixture.detectChanges();
+
+    expect(component.title).toMatch('Master Note');
+    expect(component.note.value).toMatch('test note');
+  });
+
+  test('should dismiss modal with no data', (): void => {
+    const dismissSpy: jest.SpyInstance = jest.spyOn(component.modalCtrl, 'dismiss');
+
+    fixture.detectChanges();
+
+    component.dismiss();
+    expect(dismissSpy).toHaveBeenCalled();
+  });
+
+  test('should handle note deletion modal dismiss', (): void => {
+    const dismissSpy: jest.SpyInstance = jest.spyOn(component.modalCtrl, 'dismiss');
+
+    fixture.detectChanges();
+
+    component.onDelete();
+    expect(dismissSpy).toHaveBeenCalledWith({ method: 'delete' });
+  });
+
+  test('should handle note submission', (): void => {
+    component.note = new FormControl('test note');
+    component.formMethod = 'create';
+    component.modalCtrl.dismiss = jest.fn();
+    const dismissSpy: jest.SpyInstance = jest.spyOn(component.modalCtrl, 'dismiss');
+
+    fixture.detectChanges();
+
+    component.onSubmit();
+    expect(dismissSpy).toHaveBeenCalledWith({
+      method: 'create',
+      note: 'test note'
+    });
+  });
+
+  test('should render template with note form', (): void => {
+    component.note = new FormControl('test note');
+    component.formMethod = 'update';
+
+    fixture.detectChanges();
+
+    const textarea: HTMLElement = fixture.nativeElement.querySelector('app-form-text-area');
+    expect(textarea.getAttribute('controlName')).toMatch('note');
+    const formButtons: HTMLElement = fixture.nativeElement.querySelector('app-form-buttons');
+    expect(formButtons).toBeTruthy();
+    const deleteButton: HTMLElement = fixture.nativeElement.querySelector('app-delete-button');
+    expect(deleteButton).toBeTruthy();
+  });
+
+  test('should render template with delete button removed', (): void => {
+    component.note = new FormControl('');
+    component.formMethod = 'create';
+
+    fixture.detectChanges();
+
+    const textarea: HTMLElement = fixture.nativeElement.querySelector('app-form-text-area');
+    expect(textarea.getAttribute('controlName')).toMatch('note');
+    const formButtons: HTMLElement = fixture.nativeElement.querySelector('app-form-buttons');
+    expect(formButtons).toBeTruthy();
+    const deleteButton: HTMLElement = fixture.nativeElement.querySelector('app-delete-button');
+    expect(deleteButton).toBeNull();
+  });
 
 });
