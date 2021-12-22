95c3fbe2524a796a3985c915f58c3a50e410c2b4
Andrew Wanex <awanex2705@gmail.com>
2021-12-22 13:48:47 -0800

    chore: update barrel file exports

--- src/app/components/recipe/public.ts
+++ src/app/components/recipe/public.ts
@@ -1,7 +1,3 @@
-export { GeneralFormButtonComponentModule } from './public/general-form-button/general-form-button.module';
-export { RecipeQuickDataComponentModule } from './public/recipe-quick-data/recipe-quick-data.module';
 export { RecipeSliderComponentModule } from './public/recipe-slider/recipe-slider.module';
 export { RecipeFormComponentModule } from './public/recipe-form/recipe-form.module';
-
-export { RecipeQuickDataComponent } from './public/recipe-quick-data/recipe-quick-data.component';
 export { RecipeSliderComponent } from './public/recipe-slider/recipe-slider.component';
