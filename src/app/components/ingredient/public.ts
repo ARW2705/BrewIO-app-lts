6a1eaaf4f164f086d8dd9f4792b018aa9e1522e3
Andrew Wanex <awanex2705@gmail.com>
2021-12-22 14:33:59 -0800

    chore: update ingredient barrel file exports

--- src/app/components/ingredient/public.ts
+++ src/app/components/ingredient/public.ts
@@ -1,4 +1,4 @@
-export { IngredientsContainerComponentModule } from './public/ingredients-container/ingredients-container.module';
+export { IngredientContainerComponentModule } from './public/ingredient-container/ingredient-container.module';
 export { IngredientListComponentModule } from './public/ingredient-list/ingredient-list.module';
 
 export { IngredientListComponent } from './public/ingredient-list/ingredient-list.component';
