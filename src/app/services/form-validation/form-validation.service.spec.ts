6027c329c0f30a1fec0424c6760b730464e5b057
Andrew Wanex <awanex2705@gmail.com>
2021-12-22 13:17:40 -0800

    chore: update formatting

--- src/app/services/form-validation/form-validation.service.spec.ts
+++ src/app/services/form-validation/form-validation.service.spec.ts
@@ -2,162 +2,146 @@
 import { TestBed, getTestBed } from '@angular/core/testing';
 import { FormControl, FormGroup } from '@angular/forms';
 
+/* Test configuration imports */
+import { configureTestBed } from '../../../../test-config/configure-test-bed';
+
 /* Provider imports */
 import { FormValidationService } from './form-validation.service';
 
 
-describe('Custom form validator service', () => {
+describe('Custom form validator service', (): void => {
+  configureTestBed();
   let injector: TestBed;
-  let formValidator: FormValidationService;
+  let service: FormValidationService;
 
-  beforeAll(() => {
+  beforeAll((): void => {
     TestBed.configureTestingModule({
       providers: [ FormValidationService ]
     });
-
     injector = getTestBed();
-    formValidator = injector.get(FormValidationService);
+    service = injector.get(FormValidationService);
   });
 
 
-  test('should match password and confirmation', () => {
+  test('should match password and confirmation', (): void => {
     const formGroup: FormGroup = new FormGroup({
       password: new FormControl('abcDEF123!@#'),
       passwordConfirmation: new FormControl('abcDEF123!@#')
     });
-    expect(formValidator.passwordMatch()(formGroup)).toBeNull();
-  }); // end 'should match password and confirmation' test
+    expect(service.passwordMatch()(formGroup)).toBeNull();
+  });
 
-  test('should fail matching: missing confirmation', () => {
+  test('should fail matching: missing confirmation', (): void => {
     const formGroup: FormGroup = new FormGroup({
       password: new FormControl('abcDEF123!@#'),
       passwordConfirmation: new FormControl('')
     });
-    formValidator.passwordMatch()(formGroup);
-    expect(formGroup.controls.passwordConfirmation.getError('required'))
-      .toBe(true);
-  }); // end 'should fail matching: missing confirmation' test
+    service.passwordMatch()(formGroup);
+    expect(formGroup.controls.passwordConfirmation.getError('required')).toBe(true);
+  });
 
-  test('should fail matching: mismatch', () => {
+  test('should fail matching: mismatch', (): void => {
     const formGroup: FormGroup = new FormGroup({
       password: new FormControl('abcDEF123!@#'),
       passwordConfirmation: new FormControl('abc')
     });
-    formValidator.passwordMatch()(formGroup);
-    expect(formGroup.controls.passwordConfirmation.getError('mismatch'))
-      .toBe(true);
-  }); // end 'should fail matching: mismatch' test
+    service.passwordMatch()(formGroup);
+    expect(formGroup.controls.passwordConfirmation.getError('mismatch')).toBe(true);
+  });
 
-  test('should match the required password pattern', () => {
+  test('should match the required password pattern', (): void => {
     const formControl: FormControl = new FormControl('abcDEF123');
-    expect(formValidator.passwordPattern()(formControl)).toBeNull();
-  }); // end 'should match the required password pattern' test
+    expect(service.passwordPattern()(formControl)).toBeNull();
+  });
 
-  test('should fail to match the required password pattern', () => {
+  test('should fail to match the required password pattern', (): void => {
     const formControl: FormControl = new FormControl('abcdef123');
-    expect(formValidator.passwordPattern()(formControl))
-      .toStrictEqual({ passwordInvalid: true });
-  }); // end 'should fail to match the required password pattern' test
+    expect(service.passwordPattern()(formControl)).toStrictEqual({ passwordInvalid: true });
+  });
 
-  test('should pass eitherOr validation with no additional validators', () => {
+  test('should pass eitherOr validation with no additional validators', (): void => {
     const control1: FormControl = new FormControl('');
     const control2: FormControl = new FormControl('5');
     const control3: FormControl = new FormControl('');
-
     const formGroup: FormGroup = new FormGroup({
       control1: control1,
       control2: control2,
       control3: control3
     });
 
-    formValidator.eitherOr(['control1', 'control2', 'control3'])(formGroup);
+    service.eitherOr(['control1', 'control2', 'control3'])(formGroup);
 
     expect(control1.errors).toBeNull();
     expect(control2.errors).toBeNull();
     expect(control3.errors).toBeNull();
-  }); // end 'should pass eitherOr validation with no additional validators' test
+  });
 
-  test('should pass eitherOr validation with additional validators', () => {
+  test('should pass eitherOr validation with additional validators', (): void => {
     const control1: FormControl = new FormControl('10');
     const control2: FormControl = new FormControl('5');
     const control3: FormControl = new FormControl('1');
-
     const formGroup: FormGroup = new FormGroup({
       control1: control1,
       control2: control2,
       control3: control3
     });
 
-    formValidator
-      .eitherOr(
-        ['control1', 'control2', 'control3'],
-        { required: true, min: 0 }
-      )(formGroup);
+    service.eitherOr(['control1', 'control2', 'control3'], { required: true, min: 0 })(formGroup);
 
     expect(control1.errors).toBeNull();
     expect(control2.errors).toBeNull();
     expect(control3.errors).toBeNull();
-  }); // end 'should pass eitherOr validation with additional validators' test
+  });
 
-  test('should fail eitherOr due to no controls having a value', () => {
+  test('should fail eitherOr due to no controls having a value', (): void => {
     const control1: FormControl = new FormControl('');
     const control2: FormControl = new FormControl('');
     const control3: FormControl = new FormControl('');
-
     const formGroup: FormGroup = new FormGroup({
       control1: control1,
       control2: control2,
       control3: control3
     });
 
-    formValidator.eitherOr(['control1', 'control2', 'control3'])(formGroup);
+    service.eitherOr(['control1', 'control2', 'control3'])(formGroup);
 
     expect(control1.errors).toStrictEqual({ eitherOr: true });
     expect(control2.errors).toStrictEqual({ eitherOr: true });
     expect(control3.errors).toStrictEqual({ eitherOr: true });
-  }); // end 'should fail eitherOr due to no controls having a value' test
+  });
 
-  test('should fail eitherOr due to failed additional min validation', () => {
+  test('should fail eitherOr due to failed additional min validation', (): void => {
     const control1: FormControl = new FormControl('0');
     const control2: FormControl = new FormControl('1');
     const control3: FormControl = new FormControl('2');
-
     const formGroup: FormGroup = new FormGroup({
       control1: control1,
       control2: control2,
       control3: control3
     });
 
-    formValidator
-      .eitherOr(
-        ['control1', 'control2', 'control3'],
-        { min: 3 }
-      )(formGroup);
+    service.eitherOr(['control1', 'control2', 'control3'], { min: 3 })(formGroup);
 
-    expect(control1.errors)
-      .toStrictEqual({ eitherOr: true, min: { min: 3, actual: 0 } });
-    expect(control2.errors)
-      .toStrictEqual({ eitherOr: true, min: { min: 3, actual: 1 } });
-    expect(control3.errors)
-      .toStrictEqual({ eitherOr: true, min: { min: 3, actual: 2 } });
+    expect(control1.errors).toStrictEqual({ eitherOr: true, min: { min: 3, actual: 0 } });
+    expect(control2.errors).toStrictEqual({ eitherOr: true, min: { min: 3, actual: 1 } });
+    expect(control3.errors).toStrictEqual({ eitherOr: true, min: { min: 3, actual: 2 } });
   });
 
-  test('should pass required validation if selected as such', () => {
+  test('should pass required validation if selected as such', (): void => {
     const formControl: FormControl = new FormControl('content');
-    formValidator.requiredIfValidator(true)(formControl);
+    service.requiredIfValidator(true)(formControl);
     expect(formControl.errors).toBeNull();
-  }); // end 'should pass required validation if selected as such' test
+  });
 
-  test('should pass validation if not required', () => {
+  test('should pass validation if not required', (): void => {
     const formControl: FormControl = new FormControl('');
-    formValidator.requiredIfValidator(false)(formControl);
+    service.requiredIfValidator(false)(formControl);
     expect(formControl.errors).toBeNull();
-  }); // end 'should pass validation if not required' test
+  });
 
-  test('should fail required validation', () => {
+  test('should fail required validation', (): void => {
     const formControl: FormControl = new FormControl(null);
-    expect(formValidator.requiredIfValidator(true)(formControl))
-      .toStrictEqual({ required: true });
-  }); // end 'should fail required validation' test
+    expect(service.requiredIfValidator(true)(formControl)).toStrictEqual({ required: true });
+  });
 
 });
--- src/app/services/form-validation/form-validation.service.ts
+++ src/app/services/form-validation/form-validation.service.ts
@@ -8,109 +8,96 @@ import { AbstractControl, FormGroup, ValidatorFn } from '@angular/forms';
 })
 export class FormValidationService {
 
-    constructor() { }
-
-    /**
-     * Check if at least one control from supplied controls has a value
-     *
-     * @params: controls - control names to check
-     *
-     * @return: ValidatorFn
-     */
-    eitherOr(controls: string[], additionalValidators?: object): ValidatorFn {
-      return (group: FormGroup): { [key: string]: any } | null => {
-        const controlNames: string[] = controls;
-        const hasValid: boolean = controlNames
-          .some((controlName: string): boolean => {
-            const control: AbstractControl = group.get(controlName);
-            return !isNaN(parseFloat(control.value));
-          });
-        controlNames
-          .forEach((controlName: string): void => {
-            const control: AbstractControl = group.get(controlName);
-            control.setErrors(hasValid ? null : { eitherOr: true });
-            if (hasValid && additionalValidators) {
-              for (const key in additionalValidators) {
-                if (additionalValidators.hasOwnProperty(key)) {
-                  switch (key) {
-                    case 'min':
-                      if (control.value) {
-                        const min: number = additionalValidators[key];
-                        const actual: number = parseFloat(control.value);
-                        if (isNaN(actual) || actual < min) {
-                          control.setErrors(
-                            {
-                              min: { min: min, actual: actual } ,
-                              eitherOr: true
-                            }
-                          );
-                        } else {
-                          control.setErrors(null);
-                        }
-                      }
-                      break;
-                    default:
-                      break;
+  /**
+   * Check if at least one control from supplied controls has a value
+   *
+   * @param: controls - control names to check
+   * @return: ValidatorFn
+   */
+  eitherOr(controls: string[], additionalValidators?: object): ValidatorFn {
+    return (group: FormGroup): { [key: string]: any } | null => {
+      const controlNames: string[] = controls;
+      const hasValid: boolean = controlNames.some((controlName: string): boolean => {
+        const control: AbstractControl = group.get(controlName);
+        return !isNaN(parseFloat(control.value));
+      });
+      controlNames.forEach((controlName: string): void => {
+        const control: AbstractControl = group.get(controlName);
+        control.setErrors(hasValid ? null : { eitherOr: true });
+        if (hasValid && additionalValidators) {
+          for (const key in additionalValidators) {
+            if (additionalValidators.hasOwnProperty(key)) {
+              switch (key) {
+                case 'min':
+                  if (control.value) {
+                    const min: number = additionalValidators[key];
+                    const actual: number = parseFloat(control.value);
+                    if (isNaN(actual) || actual < min) {
+                      control.setErrors({ min: { min, actual }, eitherOr: true });
+                    } else {
+                      control.setErrors(null);
+                    }
                   }
-                }
+                  break;
+                default:
+                  break;
               }
             }
-          });
-        return null;
-      };
-    }
-
-    /**
-     * Password and password confirmation matching validator
-     *
-     * @params: none
-     *
-     * @return: ValidatorFn - @params group to pull password and confirmation
-     * strings
-     */
-    passwordMatch(): ValidatorFn {
-      return (group: FormGroup): {[key: string]: any} | null => {
-        const password: AbstractControl = group.get('password');
-        const confirmation: AbstractControl = group.get('passwordConfirmation');
-
-        if (!confirmation.value) {
-          confirmation.setErrors({required: true});
-        } else if (password.value !== confirmation.value) {
-          confirmation.setErrors({mismatch: true});
+          }
         }
+      });
+      return null;
+    };
+  }
 
-        return null;
-      };
-    }
+  /**
+   * Password and password confirmation matching validator
+   *
+   * @param: none
+   * @return: ValidatorFn - @param group to pull password and confirmation strings
+   */
+  passwordMatch(): ValidatorFn {
+    return (group: FormGroup): {[key: string]: any} | null => {
+      const password: AbstractControl = group.get('password');
+      const confirmation: AbstractControl = group.get('passwordConfirmation');
+      if (!confirmation.value) {
+        confirmation.setErrors({required: true});
+      } else if (password.value !== confirmation.value) {
+        confirmation.setErrors({mismatch: true});
+      }
 
-    /**
-     * Password pattern checking
-     *
-     * @params: none
-     *
-     * @return: ValidatorFn - @params control to pull password string
-     */
-    passwordPattern(): ValidatorFn {
-      return (control: AbstractControl): {[key: string]: any} | null => {
-        return RegExp(/(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])/g)
-          .test(control.value) ? null : {passwordInvalid: true};
-      };
-    }
+      return null;
+    };
+  }
 
-    /**
-     * Conditionally set required validator
-     *
-     * @params: isRequired - true if control should be required
-     *
-     * @return: ValidatorFn - @params control to attach validator
-     */
-    requiredIfValidator(isRequired: boolean): ValidatorFn {
-      return (control: AbstractControl): { [key: string]: any } | null => {
-        if (isRequired && !control.value) {
-          return { required: true };
-        }
-        return null;
-      };
-    }
+  /**
+   * Password pattern checking
+   *
+   * @param: none
+   * @return: ValidatorFn - @param control to pull password string
+   */
+  passwordPattern(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      return RegExp(/(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])/g).test(control.value)
+        ? null
+        : { passwordInvalid: true };
+    };
+  }
+
+  /**
+   * Conditionally set required validator
+   *
+   * @param: isRequired - true if control should be required
+   * @return: ValidatorFn - @param control to attach validator
+   */
+  requiredIfValidator(isRequired: boolean): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      if (isRequired && !control.value) {
+        return { required: true };
+      }
+
+      return null;
+    };
+  }
 
 }
