432eb2e0aa8a0e64e6685a24254b98de39251989
Andrew Wanex <awanex2705@gmail.com>
2021-12-22 12:06:22 -0800

    refactor: remove unused utility functions

--- src/app/services/utility/utility.service.ts
+++ src/app/services/utility/utility.service.ts
@@ -2,10 +2,6 @@
 import { Injectable, SimpleChange } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-/* Custom Type imports */
-import { ArrayElement, CustomError } from '../../shared/types';
-
-
 @Injectable({
   providedIn: 'root'
 })
@@ -23,62 +19,13 @@ export class UtilityService {
     'updatedAt'
   ];
 
-  /***** Array Helpers *****/
-
-  /**
-   * Typescript array union workaround
-   * https://github.com/microsoft/TypeScript/issues/30271
-   * https://github.com/Microsoft/TypeScript/issues/7294#issuecomment-465794460
-   *
-   * @param: array - input array to convert
-   * @return: converted array
-   */
-  flattenArrayType<T>(array: T): ArrayElement<T>[] {
-    return array as any;
-  }
-
-  /**
-   * Perform provided callback function within a given range of numbers
-   *
-   * @param: callbackFn - callback function to execute for each number
-   * @param: stop - stop after this count (exclusive)
-   * @param: [start] - optional start number; defaults to 0
-   * @param: [step] - optional step size; defaults to 1
-   *
-   * @return: none
-   */
-  forInRange(
-    callbackFn: (index: number) => void,
-    stop: number,
-    start: number = 0,
-    step: number = 1
-  ): void {
-    if (step === 0) {
-      const message: string = 'Range step interval must not be 0';
-      const severity: number = 2;
-      throw new CustomError('ValueError', message, severity, message);
-    }
-
-    const isIncreasing: boolean = step > 0;
-    if (isIncreasing ? stop > start : stop < start) {
-      const loopStop: number = Math.abs(stop);
-      for (let i: number = start; Math.abs(i) < loopStop; i += step) {
-        callbackFn(i);
-      }
-    }
-  }
-
-  /***** End Array Helpers *****/
-
-
   /***** Object Helpers *****/
 
   /**
    * Deep copy an object - use with objects whose values follow the types
    *  Object, Array, string, number, boolean, or Date
    *
-   * @params: obj - object to copy
-   *
+   * @param: obj - object to copy
    * @return: deep copied object
    */
   clone<T>(obj: T): T {
@@ -112,9 +59,8 @@ export class UtilityService {
   /**
    * ion-select comparison function - allows objects as values
    *
-   * @params: o1 - comparison object
-   * @params: o2 - comparison object
-   *
+   * @param: o1 - comparison object
+   * @param: o2 - comparison object
    * @return: true if object ids match
    */
   compareWith(o1: any, o2: any): boolean {
@@ -128,10 +74,8 @@ export class UtilityService {
   /**
    * Determines whether an object has a property with a specified name
    *
-   *
    * @param: obj - the object to test
    * @param: key - property identifier
-   *
    * @return: true if object contains the key
    */
   hasProperty(obj: object, key: (string | number | symbol)): boolean {
@@ -144,11 +88,9 @@ export class UtilityService {
   /***** RXJS Helpers *****/
 
   /**
-   * Convert an array of behavior subjects to an array of the current value of
-   * those subjects
+   * Convert an array of behavior subjects to an array of the current value of those subjects
    *
-   * @params: subArr - an array of behavior subjects of an object
-   *
+   * @param: subArr - an array of behavior subjects of an object
    * @return: new array of the current values of each behavior subject
    */
   getArrayFromSubjects<T>(subArr: BehaviorSubject<T>[]): T[] {
@@ -158,8 +100,7 @@ export class UtilityService {
   /**
    * Convert an array into an array of behavior subjects
    *
-   * @params: array - array to convert
-   *
+   * @param: array - array to convert
    * @return: array of behavior subjects
    */
   toSubjectArray<T>(array: T[]): BehaviorSubject<T>[] {
@@ -174,9 +115,8 @@ export class UtilityService {
   /**
    * Round a number to a given decimal place
    *
-   * @params: numToRound - source number to round off
-   * @params: places - number of places to round to
-   *
+   * @param: numToRound - source number to round off
+   * @param: places - number of places to round to
    * @return: rounded off number
    */
   roundToDecimalPlace(baseNum: number, places: number): number {
@@ -190,8 +130,7 @@ export class UtilityService {
   /**
    * Remove database specific shared properties from object
    *
-   * @params: obj - object to modify
-   *
+   * @param: obj - object to modify
    * @return: none
    */
   stripSharedProperties(obj: object): void {
@@ -219,8 +158,7 @@ export class UtilityService {
   /**
    * Change string to title case
    *
-   * @params: str - string to modify
-   *
+   * @param: str - string to modify
    * @return: string in title case
    */
   toTitleCase(str: string): string {
@@ -238,8 +176,7 @@ export class UtilityService {
   /**
    * Check if new SimpleChange value has the same values as the previous SimpleChange value
    *
-   * @params: changes - changes detected from ngOnChanges
-   *
+   * @param: changes - changes detected from ngOnChanges
    * @return: true if current values are different than previous values
    */
   hasChanges(changes: SimpleChange): boolean {
@@ -247,18 +184,5 @@ export class UtilityService {
   }
 
   /***** End Lifecycle Helpers *****/
-
-
-  /***** String Helpers *****/
-
-  isUpperCase(str: string): boolean {
-    return !(new RegExp(/[a-z]+/)).test(str);
-  }
-
-  isLowerCase(str: string): boolean {
-    return !(new RegExp(/[A-Z]+/)).test(str);
-  }
-
-  /***** End String Helpers *****/
 
 }
