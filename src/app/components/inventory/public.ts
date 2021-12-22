5c5feb79c6818950d610cfae250c656692686f2f
Andrew Wanex <awanex2705@gmail.com>
2021-12-22 14:25:32 -0800

    chore: update inventory barrel file exports

--- src/app/components/inventory/public.ts
+++ src/app/components/inventory/public.ts
@@ -1,4 +1,2 @@
 export { InventoryComponentModule } from './public/inventory/inventory.module';
-export { InventoryItemDetailsFormComponentModule } from './public/inventory-item-details-form/inventory-item-details-form.module';
-export { InventoryStockDetailsFormComponentModule } from './public/inventory-stock-details-form/inventory-stock-details-form.module';
-export { InventorySupplierDetailsFormComponentModule } from './public/inventory-supplier-details-form/inventory-supplier-details-form.module';
+export { InventoryFormComponentModule } from './public/inventory-form/inventory-form.module';
