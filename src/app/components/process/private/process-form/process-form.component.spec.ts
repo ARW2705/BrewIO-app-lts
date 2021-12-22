e1389674fb09c85a317cf5a79781d202e94828be
Andrew Wanex <awanex2705@gmail.com>
2021-12-22 14:11:03 -0800

    test: update test suite to match component changes

--- src/app/components/process/private/process-form/process-form.component.spec.ts
+++ src/app/components/process/private/process-form/process-form.component.spec.ts
@@ -1,52 +1,39 @@
 /* Module imports */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonicModule, ModalController } from '@ionic/angular';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
 
 /* Test configuration imports */
 import { configureTestBed } from '../../../../../../test-config/configure-test-bed';
 
 /* Mock imports */
-import { mockProcessSchedule } from '../../../../../../test-config/mock-models';
-import { HeaderComponentStub } from '../../../../../../test-config/component-stubs';
+import { mockCalendarProcess, mockManualProcess, mockTimerProcess } from '../../../../../../test-config/mock-models';
+import { HeaderComponentStub, ProcessCalendarFormComponentStub } from '../../../../../../test-config/component-stubs';
 import { ModalControllerStub } from '../../../../../../test-config/ionic-stubs';
 import { UtilityServiceStub } from '../../../../../../test-config/service-stubs';
 
 /* Interface imports */
-import { CalendarProcess, ManualProcess, Process, TimerProcess } from '../../../../shared/interfaces';
+import { CalendarProcess, ManualProcess, TimerProcess } from '../../../../shared/interfaces';
 
 /* Service imports */
 import { UtilityService } from '../../../../services/services';
 
 /* Component imports */
+import { ProcessCalendarFormComponent } from '../process-calendar-form/process-calendar-form.component';
 import { ProcessFormComponent } from './process-form.component';
 
 
 describe('ProcessFormComponent', (): void => {
-  let fixture: ComponentFixture<ProcessFormComponent>;
-  let processFormComponent: ProcessFormComponent;
-  let originalOnInit: any;
-  const formBuilder: FormBuilder = new FormBuilder();
-  const initDefaultForm: () => FormGroup = (): FormGroup => {
-    return formBuilder.group({
-      type: '',
-      name: '',
-      description: ''
-    });
-  };
   configureTestBed();
+  let fixture: ComponentFixture<ProcessFormComponent>;
+  let component: ProcessFormComponent;
+  let originalOnInit: any;
 
   beforeAll((done: any): Promise<void> => (async (): Promise<void> => {
     TestBed.configureTestingModule({
       declarations: [
         ProcessFormComponent,
         HeaderComponentStub
-      ],
-      imports: [
-        IonicModule,
-        FormsModule,
-        ReactiveFormsModule
       ],
       providers: [
         { provide: ModalController, useClass: ModalControllerStub },
@@ -61,16 +48,156 @@ describe('ProcessFormComponent', (): void => {
 
   beforeEach((): void => {
     fixture = TestBed.createComponent(ProcessFormComponent);
-    processFormComponent = fixture.componentInstance;
-    originalOnInit = processFormComponent.ngOnInit;
-    processFormComponent.ngOnInit = jest.fn();
-    processFormComponent.modalCtrl.dismiss = jest.fn();
+    component = fixture.componentInstance;
+    originalOnInit = component.ngOnInit;
+    component.ngOnInit = jest.fn();
+    component.modalCtrl.dismiss = jest.fn();
+    component.utilService.toTitleCase = jest.fn()
+      .mockImplementation((str: string): string => str);
   });
 
   test('should create the component', (): void => {
     fixture.detectChanges();
+    expect(component).toBeDefined();
+  });
 
-    expect(processFormComponent).toBeDefined();
+  test('should init the component', (): void => {
+    component.ngOnInit = originalOnInit;
+    component.processType = 'calendar';
+    const setSpy: jest.SpyInstance = jest.spyOn(component, 'setFormValidity');
+
+    fixture.detectChanges();
+
+    expect(setSpy).not.toHaveBeenCalled();
+    expect(component.formType).toMatch('create');
+    expect(component.title).toMatch('calendar');
+  });
+
+  test('should init the component with update', (): void => {
+    component.ngOnInit = originalOnInit;
+    component.processType = 'calendar';
+    const _mockCalendarProcess: CalendarProcess = mockCalendarProcess();
+    component.update = _mockCalendarProcess;
+    const setSpy: jest.SpyInstance = jest.spyOn(component, 'setFormValidity');
+
+    fixture.detectChanges();
+
+    expect(setSpy).toHaveBeenCalledWith(true);
+    expect(component.formType).toMatch('update');
+    expect(component.title).toMatch('calendar');
+  });
+
+  test('should dismiss the modal with no data', (): void => {
+    const dismissSpy: jest.SpyInstance = jest.spyOn(component.modalCtrl, 'dismiss');
+
+    fixture.detectChanges();
+
+    component.dismiss();
+    expect(dismissSpy).toHaveBeenCalled();
+  });
+
+  test('should dismiss the modal with deletion flag', (): void => {
+    const dismissSpy: jest.SpyInstance = jest.spyOn(component.modalCtrl, 'dismiss');
+
+    fixture.detectChanges();
+
+    component.onDeletion();
+    expect(dismissSpy).toHaveBeenCalledWith({ delete: true });
+  });
+
+  test('should dismiss the modal with form values', (): void => {
+    const dismissSpy: jest.SpyInstance = jest.spyOn(component.modalCtrl, 'dismiss');
+
+    fixture.detectChanges();
+
+    const calendarForm: ProcessCalendarFormComponent = (new ProcessCalendarFormComponentStub()) as ProcessCalendarFormComponent;
+    calendarForm.getFormResult = jest.fn()
+      .mockReturnValue({ test: true });
+    component.formRef = calendarForm;
+    component.onSubmit();
+    expect(dismissSpy).toHaveBeenCalledWith({ test: true });
+  });
+
+  test('should set form validity', (): void => {
+    fixture.detectChanges();
+
+    expect(component.isFormValid).toBe(false);
+    component.setFormValidity(true);
+    expect(component.isFormValid).toBe(true);
+    component.setFormValidity(false);
+    expect(component.isFormValid).toBe(false);
+  });
+
+  test('should render the template as create form', (): void => {
+    component.ngOnInit = originalOnInit;
+    component.processType = '';
+
+    fixture.detectChanges();
+
+    const formButtons: HTMLElement = fixture.nativeElement.querySelector('app-form-buttons');
+    expect(formButtons).toBeTruthy();
+    const deleteButton: HTMLElement = fixture.nativeElement.querySelector('app-delete-button');
+    expect(deleteButton).toBeNull();
+  });
+
+  test('should render the template as a calendar form', (): void => {
+    component.ngOnInit = originalOnInit;
+    component.processType = 'calendar';
+    const _mockCalendarProcess: CalendarProcess = mockCalendarProcess();
+    component.update = _mockCalendarProcess;
+
+    fixture.detectChanges();
+
+    const formButtons: HTMLElement = fixture.nativeElement.querySelector('app-form-buttons');
+    expect(formButtons).toBeTruthy();
+    const deleteButton: HTMLElement = fixture.nativeElement.querySelector('app-delete-button');
+    expect(deleteButton).toBeTruthy();
+    const calendarForm: HTMLElement = fixture.nativeElement.querySelector('app-process-calendar-form');
+    expect(calendarForm).toBeTruthy();
+    const manualForm: HTMLElement = fixture.nativeElement.querySelector('app-process-manual-form');
+    expect(manualForm).toBeNull();
+    const timerForm: HTMLElement = fixture.nativeElement.querySelector('app-process-timer-form');
+    expect(timerForm).toBeNull();
+  });
+
+  test('should render the template as a manual form', (): void => {
+    component.ngOnInit = originalOnInit;
+    component.processType = 'manual';
+    const _mockManualProcess: ManualProcess = mockManualProcess();
+    component.update = _mockManualProcess;
+
+    fixture.detectChanges();
+
+    const formButtons: HTMLElement = fixture.nativeElement.querySelector('app-form-buttons');
+    expect(formButtons).toBeTruthy();
+    const deleteButton: HTMLElement = fixture.nativeElement.querySelector('app-delete-button');
+    expect(deleteButton).toBeTruthy();
+    const calendarForm: HTMLElement = fixture.nativeElement.querySelector('app-process-calendar-form');
+    expect(calendarForm).toBeNull();
+    const manualForm: HTMLElement = fixture.nativeElement.querySelector('app-process-manual-form');
+    expect(manualForm).toBeTruthy();
+    const timerForm: HTMLElement = fixture.nativeElement.querySelector('app-process-timer-form');
+    expect(timerForm).toBeNull();
+  });
+
+  test('should render the template as a timer form', (): void => {
+    component.ngOnInit = originalOnInit;
+    component.processType = 'timer';
+    const _mockTimerProcess: TimerProcess = mockTimerProcess();
+    component.update = _mockTimerProcess;
+
+    fixture.detectChanges();
+
+    const formButtons: HTMLElement = fixture.nativeElement.querySelector('app-form-buttons');
+    expect(formButtons).toBeTruthy();
+    const deleteButton: HTMLElement = fixture.nativeElement.querySelector('app-delete-button');
+    expect(deleteButton).toBeTruthy();
+    const calendarForm: HTMLElement = fixture.nativeElement.querySelector('app-process-calendar-form');
+    expect(calendarForm).toBeNull();
+    const manualForm: HTMLElement = fixture.nativeElement.querySelector('app-process-manual-form');
+    expect(manualForm).toBeNull();
+    const timerForm: HTMLElement = fixture.nativeElement.querySelector('app-process-timer-form');
+    expect(timerForm).toBeTruthy();
   });
 
 });
