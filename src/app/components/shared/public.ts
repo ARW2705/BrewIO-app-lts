0625172172f39aee0bad3c4e7f3612b77853a0cb
Andrew Wanex <awanex2705@gmail.com>
2021-12-22 13:44:02 -0800

    chore: udpate barrel file exports

--- src/app/components/shared/public.ts
+++ src/app/components/shared/public.ts
@@ -1,5 +1,6 @@
 export { AboutComponentModule } from './about/about.module';
 export { AccordionComponentModule } from './accordion/accordion.module';
+export { ConfirmationComponentModule } from './confirmation/confirmation.module';
 export { DeleteButtonComponentModule } from './delete-button/delete-button.module';
 export { HeaderComponentModule } from './header/header.module';
 export { LoadingSpinnerComponentModule } from './loading-spinner/loading-spinner.module';
@@ -7,6 +8,7 @@ export { PreferencesComponentModule } from './preferences/preferences.module';
 
 export { AboutComponent } from './about/about.component';
 export { AccordionComponent } from './accordion/accordion.component';
+export { ConfirmationComponent } from './confirmation/confirmation.component';
 export { DeleteButtonComponent } from './delete-button/delete-button.component';
 export { HeaderComponent } from './header/header.component';
 export { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
